a0cee9af33938ee681be06d73757005233d9316f
Monica <ramirez.92.monica@gmail.com>
2022-08-31 17:48:10 -0500

    created js file

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log(sleepy head);
+
+let username = prompt ("What' your name?");
+let message; 
+
+if (username== "Monica")
+{message= "hello, again"}
+
+document.write (message)
