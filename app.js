cd614c3e6e3457ffce66d438eb722b0e54e99b07
Monica <ramirez.92.monica@gmail.com>
2022-09-01 15:17:34 -0500

    added functions

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 'use strict';
 
 let username = prompt("What's your name?");
-let message
-
-if (username== "Monica"){
+  let message;
+  
+function greeting(){ 
+  if (username== "Monica"){
   message= "Hello, master! "}
-else {message= "Welcome, "+username+"!"};
+  else {message= "Welcome, "+username+"!"};
+  document.write(message)};
+
+  greeting(message);
+
+ 
+
+
+  function goodbye(){
+    document.write("Thanks for visiting, "
+    +username+"!")
+   };
+
+  
+
+
+
+
+
 ;
 
-let answer= prompt("Are you a night owl or an early bird?");
-let response;
+//function namne, parameters, code to execute
+// function dothething(){  
+//   let answer= prompt("Are you a night owl or an early bird?");
+//   document.write(zzz)};
 
-if (answer == "early bird"){
-    response= " (｡◕‿◕｡) "
-}
-else{response=" (∪｡∪)｡｡｡zzz " };
-
-document.write(message);
-
-document.write(response);
+//   dothething();
 
