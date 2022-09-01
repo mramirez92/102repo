9d3e77af083b6d2cfbcd6c5a640161d44a67bbca
Monica <ramirez.92.monica@gmail.com>
2022-09-01 10:04:41 -0500

    added one more let variable and conditional statements

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ let username = prompt("What's your name?");
 let message
 
 if (username== "Monica"){
-  message= "hello, master"}
+  message= "Hello, master! "}
 else {message= "Welcome, "+username+"!"};
+;
+
+let answer= prompt("Are you a night owl or an early bird?");
+let response;
+
+if (answer == "early bird"){
+    response= " (｡◕‿◕｡) "
+}
+else{response=" (∪｡∪)｡｡｡zzz " };
+
 document.write(message);
 
+document.write(response);
 
